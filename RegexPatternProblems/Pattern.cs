5c3e6a1bf935de5ab1945b76b52f7d9bc7938f06
nk321626 <nk321626@gmail.com>
2022-05-18 18:25:31 +0530

    Adding: User to enter a valid last name UC2

--- RegexPatternProblems/Pattern.cs
+++ RegexPatternProblems/Pattern.cs
@@ -9,13 +9,22 @@ namespace RegexPatternProblems
 {
     public class Pattern
     {
+        //Create a pattern for a first name thats start with cap and has min 3 char (UC1)
         public static string FIRST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";//Minimum 3 Char.
 
-        //Method for Validate First Name.
-        public bool ValidateFirstName(string fname)
+        //Method for Validate First Name using regex(UC1)
+        public bool validateFirstName(string fname)
         {
             return Regex.IsMatch(fname, FIRST_NAME_REGEX);
 
+        }
+        //Create a pattern for a last name thats start with cap and has min 3 char (UC2)
+        public static string LAST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";//Minimum 3 Char.
+        
+        //Method for validate Last Name using regex (UC2)
+        public bool validateLastName(string lname)
+        {
+            return Regex.IsMatch(lname, LAST_NAME_REGEX);
         }
     }
 }
--- RegexPatternProblems/Program.cs
+++ RegexPatternProblems/Program.cs
@@ -3,21 +3,26 @@ namespace RegexPatternProblems
 {
     class program
     {
-        public static string instr = Console.ReadLine();
+        //Takes an input from the user.
+        private static readonly string instr = Console.ReadLine();
+        //Main method
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registation Problems");
             Pattern program = new Pattern();
             Console.WriteLine("-->Select to Program is Executed<--");
-            Console.WriteLine("1.validate First Name : \n Exit");
+            Console.WriteLine("1.Validate First Name : \n2. validate Last Name:\n3. Exit");
             Console.WriteLine("Select Above Number:->");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
-                    Console.WriteLine(program.ValidateFirstName(instr));
+                    Console.WriteLine(program.validateFirstName(instr)); //Print the output
                     break;
-                    case 2:
+                case 2:
+                    Console.WriteLine(program.validateLastName(instr)); //Print the output
+                    break;
+                case 3:
                 default:
                     Console.WriteLine("Please Select valid Number");
                     break;
