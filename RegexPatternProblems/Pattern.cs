0cf5544ddfe04934dedb33ac126c12d6194418b3
nk321626 <nk321626@gmail.com>
2022-05-18 17:24:36 +0530

    Added: Enter a valid First Name UC1

---
+++ RegexPatternProblems/Pattern.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPatternProblems
+{
+    public class Pattern
+    {
+        public static string FIRST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";//Minimum 3 Char.
+
+        //Method for Validate First Name.
+        public bool ValidateFirstName(string fname)
+        {
+            return Regex.IsMatch(fname, FIRST_NAME_REGEX);
+
+        }
+    }
+}
--- RegexPatternProblems/Program.cs
+++ RegexPatternProblems/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to User Registation Problem!");
+﻿using System;
+namespace RegexPatternProblems
+{
+    class program
+    {
+        public static string instr = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registation Problems");
+            Pattern program = new Pattern();
+            Console.WriteLine("-->Select to Program is Executed<--");
+            Console.WriteLine("1.validate First Name : \n Exit");
+            Console.WriteLine("Select Above Number:->");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine(program.ValidateFirstName(instr));
+                    break;
+                    case 2:
+                default:
+                    Console.WriteLine("Please Select valid Number");
+                    break;
+            }
+        }
+    }
+}
